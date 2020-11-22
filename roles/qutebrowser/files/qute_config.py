a88c4e1f44da55f11419cbf2e113c7e1e3041bf2
d0ku <darthtyranus666666@gmail.com>
2020-11-22 17:23:29 +0100

    [qutebrowser] add dictcc URL support, vaapi decoding to mpv calls and remove transition `translate` URL

--- roles/qutebrowser/files/qute_config.py
+++ roles/qutebrowser/files/qute_config.py
@@ -1,15 +1,15 @@
 # STREAMING/DOWNLOADING VIDEO. 
 # Max 480p.
-config.bind('<z>', 'spawn -d mpv --ytdl-format=best[height<=?480] {url}')
+config.bind('<z>', 'spawn -d mpv --hwdec=vaapi --ytdl-format=best[height<=?480] {url}')
 
 # Max 480p (ASCII).
-config.bind('<Shift-Ctrl-z>', 'spawn -d mpv --ytdl-format=best[height<=?480] {url} -vo caca')
+config.bind('<Shift-Ctrl-z>', 'spawn -d mpv --hwdec=vaapi --ytdl-format=best[height<=?480] {url} -vo caca')
 
 # Max 720p.
-config.bind('<Shift-z>', 'spawn -d mpv --ytdl-format=best[height<=?720] {url}')
+config.bind('<Shift-z>', 'spawn -d mpv --hwdec=vaapi --ytdl-format=best[height<=?720] {url}')
 
-# Max 1080p.
-config.bind('<Shift-x>', 'spawn -d mpv --ytdl-format=best[height<=?1080] {url}')
+# Uncapped, should be max quality.
+config.bind('<Shift-x>', 'spawn -d mpv --hwdec=vaapi --ytdl-format=best[height<=?1080] {url}')
 
 # Download Max 720p.
 config.bind('<Shift-Ctrl-x>', 'spawn --userscript ~/.config/qutebrowser/scripts/download_video.sh')
@@ -42,13 +42,13 @@ googlestr = "https://google.com/search?hl=en&q={}"
 # Translates english to polish by default, change to your like.
 translate_english_polish = "https://translate.google.com/#en/pl/{}"
 translate_german_polish = "https://translate.google.com/#de/pl/{}"
+dictcc_german_english = "https://dict.cc/?s={}"
 c.url.searchengines = {"DEFAULT":duckstr,
                        "ddg":duckstr,
                        "google":googlestr,
-                       # Don't remove original `translate` for transition period.
-                       "translate":translate_english_polish,
                        "english":translate_english_polish,
                        "german":translate_german_polish,
+                       "dictcc": dictcc_german_english,
 }
 
 # Fonts
