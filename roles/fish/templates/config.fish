dad295c2601fee5431a27b09b26311abff37ead6
dezeroku <dezeroku@gmail.com>
2023-07-02 01:25:42 +0200

    fish: better plugins layout

--- roles/fish/templates/config.fish
+++ roles/fish/templates/config.fish
@@ -1,14 +1,5 @@
-# PLUGINS
-fundle plugin 'edc/bass'
-fundle plugin 'danhper/fish-fastdir'
-#fundle plugin 'danhper/fish-ssh-agent'
-#Currently setting up ssh-agent is done with keychain in custom on each node.
-fundle plugin 'oh-my-fish/theme-bobthefish'
-fundle plugin 'evanlucas/fish-kubectl-completions'
-fundle plugin 'decors/fish-colors'
-
-fundle init
-
+# Set up plugins
+source ~/.config/fish/plugins.fish
 
 fish_vi_key_bindings
 fzf_key_bindings
