bf60b7a7eabcb898f23ddb38ba96434c7ef4b9e2
dezeroku <dezeroku@gmail.com>
2023-08-06 23:07:03 +0200

    fish: get kubectl completions directly

--- roles/fish/templates/plugins.fish
+++ roles/fish/templates/plugins.fish
@@ -1,10 +1,6 @@
-# PLUGINS
 fundle plugin 'edc/bass'
 fundle plugin 'danhper/fish-fastdir'
-#fundle plugin 'danhper/fish-ssh-agent'
-#Currently setting up ssh-agent is done with keychain in custom on each node.
 fundle plugin 'oh-my-fish/theme-bobthefish'
-fundle plugin 'evanlucas/fish-kubectl-completions'
 fundle plugin 'decors/fish-colors'
 
 fundle init
--- roles/kubectl_stuff/files/kubernetes.fish
+++ roles/kubectl_stuff/files/kubernetes.fish
@@ -7,3 +7,6 @@ set -gx PATH $PATH $HOME/.krew/bin
 
 # Enable completion for k9s
 # k9s completion fish | source
+
+# Enable completion for kubectl
+kubectl completion fish | source
