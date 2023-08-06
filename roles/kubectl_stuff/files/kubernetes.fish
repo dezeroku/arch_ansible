87f071ec77806f77d8a7b0a88858751a71eba9f3
dezeroku <dezeroku@gmail.com>
2023-08-06 23:51:56 +0200

    kubectl_stuff: properly generate completions for k9s and kubectl

--- roles/kubectl_stuff/files/kubernetes.fish
+++ roles/kubectl_stuff/files/kubernetes.fish
@@ -4,9 +4,3 @@ alias tf="terraform"
 
 # Plugins for kubectl
 set -gx PATH $PATH $HOME/.krew/bin
-
-# Enable completion for k9s
-# k9s completion fish | source
-
-# Enable completion for kubectl
-kubectl completion fish | source
