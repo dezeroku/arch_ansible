a002145c04002e3e8719364ef81045422d5b19cc
dezeroku <dezeroku@gmail.com>
2023-08-21 01:18:04 +0200

    youtube_dl: switch to yt-dlp

--- roles/qutebrowser/files/download_video.sh
+++ roles/qutebrowser/files/download_video.sh
@@ -4,5 +4,5 @@ url="$QUTE_URL"
 
 echo "message-info 'video-dl: Started downloading $url'" >> "${QUTE_FIFO}"
 # shellcheck disable=SC2088
-youtube-dl -f 'best[height<=?720p]' --all-subs --convert-subs srt -o '~/Videos/%(title)s.%(ext)s' "${url}" && echo "message-info 'video-dl: Finished downloading $url'" >> "${QUTE_FIFO}" && exit 0
+yt-dlp -f 'best[height<=?720p]' --all-subs --convert-subs srt -o '~/Videos/%(title)s.%(ext)s' "${url}" && echo "message-info 'video-dl: Finished downloading $url'" >> "${QUTE_FIFO}" && exit 0
 echo "message-error 'video-dl: Error while downloading $url'" >> "${QUTE_FIFO}"
