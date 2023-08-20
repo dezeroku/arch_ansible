7363b1725422f78509ed638b9bf0e5d4e4234793
dezeroku <dezeroku@gmail.com>
2023-08-21 01:22:07 +0200

    qutebrowser: more verbose download_video.sh + 1080p

--- roles/qutebrowser/files/download_video.sh
+++ roles/qutebrowser/files/download_video.sh
@@ -4,5 +4,5 @@ url="$QUTE_URL"
 
 echo "message-info 'video-dl: Started downloading $url'" >> "${QUTE_FIFO}"
 # shellcheck disable=SC2088
-yt-dlp -f 'best[height<=?720p]' --all-subs --convert-subs srt -o '~/Videos/%(title)s.%(ext)s' "${url}" && echo "message-info 'video-dl: Finished downloading $url'" >> "${QUTE_FIFO}" && exit 0
+yt-dlp -f 'bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' --all-subs --convert-subs srt -o '~/Videos/%(title)s.%(ext)s' "${url}" && echo "message-info 'video-dl: Finished downloading $url'" >> "${QUTE_FIFO}" && echo "message-info 'video-dl: Finished downloading $url'" && exit 0
 echo "message-error 'video-dl: Error while downloading $url'" >> "${QUTE_FIFO}"
