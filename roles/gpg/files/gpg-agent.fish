0087d55c7d44c698892501fd2ba3313d8082747b
dezeroku <dezeroku@gmail.com>
2023-08-23 18:13:33 +0200

    gpg: improve pinentry selection

--- roles/gpg/files/gpg-agent.fish
+++ roles/gpg/files/gpg-agent.fish
@@ -4,17 +4,28 @@ if status is-interactive
     set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
     gpg-connect-agent updatestartuptty /bye >/dev/null
 
-    if [ -n "$DISPLAY" ]
-        set -x PINENTRY_USER_DATA "USE_GTK2"
-    end
-
-    # Force the SSH_CLIENT to always use TTY,
-    # even if X is forwarded
-    if [ -n "$SSH_CLIENT" ]
-        set -x PINENTRY_USER_DATA "USE_TTY"
-    end
-
-    function tmux_name --on-event fish_preexec
+    function gpg_agent_prepare --on-event fish_preexec
         gpg-connect-agent updatestartuptty /bye >/dev/null
+
+        set -gx PINENTRY_USER_DATA "USE_TTY"
+
+        if [ -n "$TMUX" ]
+            # Refresh the variables
+            # TODO: This will still leave the tmux in a "fighting state"
+            # between variables from SSH and local.
+            # The rule of thumb here is that the one connected later takes the lead
+            # So in case of issue, reattaching to the session is recommended
+            bass (tmux showenv -s)
+        end
+
+        if [ -n "$DISPLAY" ]
+            set -gx PINENTRY_USER_DATA "USE_GTK2"
+        end
+
+        # Force the SSH_CLIENT to always use TTY,
+        # even if X is forwarded
+        if [ -n "$SSH_CLIENT" ]
+            set -gx PINENTRY_USER_DATA "USE_TTY"
+        end
     end
 end
