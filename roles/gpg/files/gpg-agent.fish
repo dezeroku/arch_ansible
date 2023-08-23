619a49c9a8046e4cdf5aa0c2ce9fedff419e47dc
dezeroku <dezeroku@gmail.com>
2023-08-23 17:06:58 +0200

    gpg: properly display pinentry over SSH and X

--- roles/gpg/files/gpg-agent.fish
+++ roles/gpg/files/gpg-agent.fish
@@ -1,6 +1,20 @@
 if status is-interactive
     set -x GPG_TTY (tty)
+    set -e SSH_AGENT_PID
     set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-    gpgconf --launch gpg-agent
-    echo "UPDATESTARTUPTTY" | gpg-connect-agent > /dev/null
+    gpg-connect-agent updatestartuptty /bye >/dev/null
+
+    if [ -n "$DISPLAY" ]
+        set -x PINENTRY_USER_DATA "USE_GTK2"
+    end
+
+    # Force the SSH_CLIENT to always use TTY,
+    # even if X is forwarded
+    if [ -n "$SSH_CLIENT" ]
+        set -x PINENTRY_USER_DATA "USE_TTY"
+    end
+
+    function tmux_name --on-event fish_preexec
+        gpg-connect-agent updatestartuptty /bye >/dev/null
+    end
 end
