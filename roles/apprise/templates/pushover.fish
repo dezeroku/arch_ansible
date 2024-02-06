227479bb4581a8521cf170524f850a6d95e3925c
dezeroku <dezeroku@gmail.com>
2024-02-06 11:01:40 +0100

    apprise: improve the dez_pushover scriptlet

--- roles/apprise/templates/pushover.fish
+++ roles/apprise/templates/pushover.fish
@@ -1,3 +1,11 @@
 function dez_pushover
-    apprise pover://{{ apprise_pushover_user_key }}@{{ apprise_pushover_api_key }} $argv
+    set argc (count $argv)
+    if test $argc -eq 0
+        echo "dez_pushover [BODY] [apprise args]"
+        return 1
+    else if test $argc -eq 1
+        apprise pover://{{ apprise_pushover_user_key }}@{{ apprise_pushover_api_key }} -b $argv
+    else
+        apprise pover://{{ apprise_pushover_user_key }}@{{ apprise_pushover_api_key }} $argv
+    end
 end
