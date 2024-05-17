4fccd2dc7a944ac2685ab69c44f4b49fecf4b620
dezeroku <contact@dezeroku.com>
2024-05-17 20:15:42 +0200

    mailctl: replace with oama

--- roles/vdirsyncer/files/oauth-oama.sh
+++ roles/vdirsyncer/files/oauth-oama.sh
@@ -11,11 +11,11 @@ TOKEN_FILE="$TOKEN_DIR/$MAIL_ADDRESS"
 touch "$TOKEN_FILE"
 chmod 0600 "$TOKEN_FILE"
 
-mailctl access "$MAIL_ADDRESS" > /dev/null
+oama access "$MAIL_ADDRESS" > /dev/null
 
-# Using mailctl access directly will not work
+# Using oama access directly will not work
 # as we need to access the whole JSON here
 # Thus we get it from gnome-keyring in the expanded format
-secret-tool lookup mailctl "$MAIL_ADDRESS" > "$TOKEN_FILE"
+secret-tool lookup oama "$MAIL_ADDRESS" > "$TOKEN_FILE"
 
 echo "$TOKEN_FILE"
